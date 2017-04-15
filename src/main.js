c9d5095385aef0423c5f217fad6e5d74c8527752
Rikard Legge <rikard.legge@gmail.com>
2017-04-15 19:57:54 +0200

    Add reference tests for created objects

--- src/main.js
+++ src/main.js
@@ -11,11 +11,13 @@ const a1 = load(A, context);
 const a2 = load(A, context);
 const a3 = load(A, {[D]: 'test'});
 
+assert.equal(a1, a2);
 assert.equal(a1.d, a2.d);
 assert.equal(a1.b, a2.b);
 assert.equal(a1.c, a2.c);
 assert.equal(a1.b.c.d, a2.b.c.d);
 
+assert.notEqual(a1, a3);
 assert.notEqual(a1.d, a3.d);
 assert.notEqual(a1.b, a3.b);
 assert.notEqual(a1.c, a3.c);
