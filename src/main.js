6a2e072abfa6da0f610d82f9bc2c10c7384ab073
Rikard Legge <rikard.legge@gmail.com>
2017-04-16 13:55:44 +0200

    Remove assertions from main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import {load} from "Injector";
+import {preferences, LOGLEVEL} from "di/Log";
 import A from "example/A";
 import D from "example/D";
-import assert from "./assert";
-import Context from "./di/Context";
+import Context from "di/Context";
+
+preferences.logLevel = LOGLEVEL.debug;
 
 const d = load(D, {name:'injected instance'});
 const context = new Context({[D]: d});
@@ -10,15 +12,3 @@ const context = new Context({[D]: d});
 const a1 = load(A, context);
 const a2 = load(A, context);
 const a3 = load(A, {[D]: 'test'});
-
-assert.equal(a1, a2);
-assert.equal(a1.d, a2.d);
-assert.equal(a1.b, a2.b);
-assert.equal(a1.c, a2.c);
-assert.equal(a1.b.c.d, a2.b.c.d);
-
-assert.notEqual(a1, a3);
-assert.notEqual(a1.d, a3.d);
-assert.notEqual(a1.b, a3.b);
-assert.notEqual(a1.c, a3.c);
-assert.notEqual(a1.b.c.d, a3.b.c.d);
