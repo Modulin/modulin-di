1c1e282e82351b725f96668f440f7177eae6e84a
Rikard Legge <rikard.legge@gmail.com>
2017-04-29 00:27:30 +0200

    Clean up test boot

--- tests/boot.js
+++ tests/boot.js
@@ -1,18 +1,13 @@
 import { main, root } from "setup";
 
 const load = modulinFetch.createLoader(root);
-load(["Injector", "InjectorConfig"], function(...args) {
-  boot(...args);
-  loadTests();
-});
-
-function boot(
-  { exports: { configurableInjector } },
-  { exports: { default: injectorConfig } }
-) {
-  configurableInjector.setConfig(injectorConfig);
-}
-
-function loadTests() {
-  load(main);
-}
+load(
+  ["Injector", "InjectorConfig"],
+  (
+    { exports: { configurableInjector } },
+    { exports: { default: injectorConfig } }
+  ) => {
+    configurableInjector.setConfig(injectorConfig);
+    load(main);
+  }
+);
